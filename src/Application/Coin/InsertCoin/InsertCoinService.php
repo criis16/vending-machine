0cc2a2c3d8adca83f69dacbe05bb4fc238d5882c
Cristian Ayala Fernandez <cristian.ayala96@gmail.com>
2024-10-18 20:23:24 +0200

    Second approach to the action insert coin

---
+++ src/Application/Coin/InsertCoin/InsertCoinService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Application\Coin\InsertCoin;
+
+use App\Domain\Coin\Coin;
+use App\Domain\Coin\CoinValue;
+use App\Domain\Coin\CoinQuantity;
+use App\Domain\Coin\Repositories\CoinRepositoryInterface;
+use App\Infrastructure\Status\Repositories\InsertBalanceRequest;
+
+class InsertCoinService
+{
+    private const INITIAL_COIN_QUANTITY = 1;
+
+    private CoinRepositoryInterface $repository;
+
+    public function __construct(
+        CoinRepositoryInterface $repository
+    ) {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Inserts a coin
+     *
+     * @param InsertBalanceRequest $request
+     * @return boolean
+     */
+    public function execute(
+        InsertBalanceRequest $request
+    ): bool {
+        $isOperationDone = false;
+        $coinQuantity = new CoinQuantity(self::INITIAL_COIN_QUANTITY);
+        $coinValue = new CoinValue($request->getCoin());
+        $coin = $this->repository->getCoinByValue($coinValue);
+
+        if (empty($coin)) {
+            $coin = new Coin($coinValue, $coinQuantity);
+            $isOperationDone = $this->repository->saveCoin($coin);
+        } else {
+            $coin = \reset($coin);
+            $coinId = $coin->getCoinId();
+            $isOperationDone = $this->repository->updateCoinQuantity($coinId, $coinQuantity);
+        }
+
+        return $isOperationDone;
+    }
+}
--- src/Infrastructure/Status/InsertBalanceController.php
+++ src/Infrastructure/Status/InsertBalanceController.php
@@ -5,6 +5,7 @@ namespace App\Infrastructure\Status;
 use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Application\Coin\InsertCoin\InsertCoinService;
 use App\Application\Status\GetBalance\GetBalanceService;
 use App\Infrastructure\Shared\ValidateRequestDataService;
 use App\Application\Status\InsertBalance\InsertBalanceService;
@@ -20,17 +21,20 @@ class InsertBalanceController
     private GetBalanceService $getBalanceService;
     private InsertBalanceRequest $request;
     private ValidateRequestDataService $validator;
+    private InsertCoinService $insertCoinService;
 
     public function __construct(
         InsertBalanceService $insertBalanceService,
         GetBalanceService $getBalanceService,
         InsertBalanceRequest $request,
-        ValidateRequestDataService $validator
+        ValidateRequestDataService $validator,
+        InsertCoinService $insertCoinService
     ) {
         $this->insertBalanceService = $insertBalanceService;
         $this->getBalanceService = $getBalanceService;
         $this->request = $request;
         $this->validator = $validator;
+        $this->insertCoinService = $insertCoinService;
     }
 
     /**
@@ -41,29 +45,32 @@ class InsertBalanceController
      */
     public function insertBalance(Request $request): JsonResponse
     {
-        $isSaved = false;
+        $isCoinSaved = false;
+        $isBalanceSaved = false;
         $statusCode = 400;
-        $balance = 0.0;
 
         try {
             $requestBody = $request->request->all();
             $this->validator->validate($requestBody, self::REQUIRED_BODY_FIELDS);
             $this->setRequestData($requestBody);
-            $isSaved = $this->insertBalanceService->execute($this->request);
+
+            $isCoinSaved = $this->insertCoinService->execute($this->request);
+            $isBalanceSaved = $this->insertBalanceService->execute($this->request);
         } catch (Exception $e) {
             $responseMessage = $e->getMessage();
         }
 
-        if ($isSaved) {
+        if ($isCoinSaved && $isBalanceSaved) {
             $statusCode = 200;
             $responseMessage = 'Coin has been inserted correctly';
-            $balance = $this->getBalanceService->execute();
         }
+
+        $currentBalance = $this->getBalanceService->execute();
 
         return new JsonResponse(
             [
                 'message' => $responseMessage,
-                'result' => 'The current balance is ' . \number_format($balance, 2)
+                'result' => 'The current balance is ' . \number_format($currentBalance, 2)
             ],
             $statusCode
         );
