254f9eeaaea11a75ead735597075772f2d5e92fc
criis16 <cristian.ayala96@gmail.com>
2024-10-19 11:07:05 +0200

    New controller to return the inserted coins

---
+++ src/Infrastructure/Coin/GetCoinsBackController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Infrastructure\Coin;
+
+use Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Application\Status\GetBalance\GetBalanceService;
+use App\Application\Coin\GetCoinsBack\GetCoinsBackService;
+use App\Application\Coin\UpdateCoinQuantity\UpdateCoinQuantityService;
+use App\Application\Status\UpdateBalance\UpdateBalanceService;
+
+class GetCoinsBackController
+{
+    private const EMPTY_BALANCE = 0.0;
+    private const EMPTY_RETURN_COINS = 'Coins returned: 0.00';
+    private const EPSILON = 0.00001;
+    private const ALLOWED_RETURN_COINS = [
+        1.00,
+        0.25,
+        0.10,
+        0.05
+    ];
+
+    private GetBalanceService $getBalanceService;
+    private GetCoinsBackService $getCoinsBackService;
+    private UpdateBalanceService $updateBalanceService;
+    private UpdateCoinQuantityService $updateCoinQuantityService;
+
+    public function __construct(
+        GetBalanceService $getBalanceService,
+        GetCoinsBackService $getCoinsBackService,
+        UpdateBalanceService $updateBalanceService,
+        UpdateCoinQuantityService $updateCoinQuantityService
+    ) {
+        $this->getBalanceService = $getBalanceService;
+        $this->getCoinsBackService = $getCoinsBackService;
+        $this->updateBalanceService = $updateBalanceService;
+        $this->updateCoinQuantityService = $updateCoinQuantityService;
+    }
+
+    public function getCoinsBack(): JsonResponse
+    {
+        $statusCode = 400;
+        $coinsToReturn = [];
+
+        try {
+            $currentBalance = $this->getBalanceService->execute();
+            $coinsToReturn = $this->getCoinsBackService->execute(self::ALLOWED_RETURN_COINS, $currentBalance);
+
+            if ($currentBalance === self::EMPTY_BALANCE) {
+                $responseMessage = 'The balance is empty. Please insert coins first.';
+            } elseif (!$this->validateReturnedCoins($coinsToReturn, $currentBalance)) {
+                $coinsToReturn = [];
+                $responseMessage = 'The vending machine has not enough coins to return.';
+            } else {
+                $areCoinsUpdated = $this->updateCoinsQuantity($coinsToReturn);
+                $isBalanceUpdated = $this->updateBalanceService->execute(self::EMPTY_BALANCE);
+
+                if ($areCoinsUpdated && $isBalanceUpdated) {
+                    $statusCode = 200;
+                    $responseMessage = 'The entered balance ' .
+                        \number_format($currentBalance, 2) .
+                        ' has been returned successfully.';
+                }
+            }
+        } catch (Exception $e) {
+            $responseMessage = $e->getMessage();
+        }
+
+        return new JsonResponse(
+            [
+                'message' => $responseMessage,
+                'result' => $this->getResponseMessage($coinsToReturn)
+            ],
+            $statusCode
+        );
+    }
+
+    /**
+     * Updates the coins quantity and returns the result status
+     *
+     * @param array $coinsToReturn
+     * @return boolean
+     */
+    private function updateCoinsQuantity(array $coinsToReturn): bool
+    {
+        foreach ($coinsToReturn as $coinValue => $coinQuantityToReturn) {
+            $isUpdated = $this->updateCoinQuantityService->execute($coinValue, $coinQuantityToReturn);
+
+            if (!$isUpdated) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns the response message
+     *
+     * @param array $coinsToReturn
+     * @return string
+     */
+    private function getResponseMessage(array $coinsToReturn): string
+    {
+        if (empty($coinsToReturn)) {
+            return self::EMPTY_RETURN_COINS;
+        }
+
+        $message = '';
+
+        foreach ($coinsToReturn as $coinValue => $coinQuantity) {
+            if ($coinQuantity === 0) {
+                continue;
+            }
+
+            $message .= \str_repeat($coinValue . ', ', $coinQuantity);
+        }
+
+        if (empty($message)) {
+            $message = self::EMPTY_RETURN_COINS;
+        } else {
+            $message = 'Coins returned: ' . \rtrim($message, ', ');
+        }
+
+        return $message;
+    }
+
+    /**
+     * Validates if the returned coins are equal to the current balance
+     *
+     * @param array $coinsToReturn
+     * @param float $currentBalance
+     * @return boolean
+     */
+    private function validateReturnedCoins(array $coinsToReturn, float $currentBalance): bool
+    {
+        $totalReturned = self::EMPTY_BALANCE;
+
+        foreach ($coinsToReturn as $coinValue => $coinQuantity) {
+            $totalReturned += $coinValue * $coinQuantity;
+        }
+
+        return \abs($totalReturned - $currentBalance) < self::EPSILON;
+    }
+}
