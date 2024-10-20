f9c04266a69ae6b4b44e4431282f91e5c2d7cc82
criis16 <cristian.ayala96@gmail.com>
2024-10-20 19:55:42 +0200

    Add price control

--- src/Domain/Item/ItemPrice.php
+++ src/Domain/Item/ItemPrice.php
@@ -4,11 +4,13 @@ namespace App\Domain\Item;
 
 class ItemPrice
 {
+    private const ZERO_PRICE = 0.0;
+
     private float $price;
 
     public function __construct(float $price)
     {
-        $this->price = $price;
+        $this->price = ($price < self::ZERO_PRICE) ? self::ZERO_PRICE : $price;
     }
 
     /**
@@ -19,5 +21,10 @@ class ItemPrice
     public function getValue(): float
     {
         return $this->price;
+    }
+
+    public function setValue(float $price): void
+    {
+        $this->price = ($price < self::ZERO_PRICE) ? self::ZERO_PRICE : $price;
     }
 }
