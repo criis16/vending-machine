25c252c1b315e5ff2ac542b8a197b6b2ee1867e2
Cristian Ayala Fernandez <cristian.ayala96@gmail.com>
2024-10-20 15:01:12 +0200

    New service to get an item by name

---
+++ src/Application/Item/GetItemByName/GetItemByNameService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Application\Item\GetItemByName;
+
+use App\Application\Item\Adapters\ItemAdapter;
+use App\Domain\Item\Item;
+use App\Domain\Item\ItemName;
+use App\Domain\Item\Repositories\ItemRepositoryInterface;
+use App\Infrastructure\Item\Repositories\InsertItemRequest;
+use InvalidArgumentException;
+
+class GetItemByNameService
+{
+    private ItemRepositoryInterface $repository;
+    private ItemAdapter $adapter;
+
+    public function __construct(
+        ItemRepositoryInterface $repository,
+        ItemAdapter $adapter
+    ) {
+        $this->repository = $repository;
+        $this->adapter = $adapter;
+    }
+
+    /**
+     * Returns the item with the given name
+     *
+     * @param InsertItemRequest $request
+     * @return array
+     */
+    public function execute(InsertItemRequest $request): array
+    {
+        $itemName = new ItemName($request->getName());
+        $items = $this->repository->getItemByName($itemName);
+
+        if (empty($items)) {
+            throw new InvalidArgumentException('The item with that name does not exist.');
+        }
+
+        return \array_map(
+            function (Item $item) {
+                return $this->adapter->adapt($item);
+            },
+            $items
+        );
+    }
+}
