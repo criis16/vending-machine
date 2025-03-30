0f8e5fcb4e9d9d92bd356182dd26e3610c8b9100
criis16 <cristian.ayala96@gmail.com>
2025-03-30 09:29:01 +0200

    Refactor InsertCoinService to use CreateCoinService and UpdateCoinQuantityService; add exception handling for unsaved coins

--- src/Application/Coin/InsertCoin/InsertCoinService.php
+++ src/Application/Coin/InsertCoin/InsertCoinService.php
@@ -2,38 +2,40 @@
 
 namespace App\Application\Coin\InsertCoin;
 
-use App\Application\Coin\GetCoinByValue\GetCoinByValueService;
-use App\Domain\Coin\Coin;
-use App\Domain\Coin\CoinId;
-use App\Domain\Coin\CoinValue;
-use App\Domain\Coin\CoinQuantity;
-use App\Domain\Coin\Repositories\CoinRepositoryInterface;
+use App\Application\Coin\CreateCoin\CreateCoinService;
+use App\Application\Coin\Exceptions\CoinNotSavedException;
 use App\Infrastructure\Coin\Repositories\InsertCoinRequest;
+use App\Application\Coin\GetCoinByValue\GetCoinByValueService;
+use App\Application\Coin\UpdateCoinQuantity\UpdateCoinQuantityService;
 
 class InsertCoinService
 {
     private const INITIAL_COIN_QUANTITY = 1;
 
-    private CoinRepositoryInterface $repository;
     private GetCoinByValueService $getCoinByValueService;
+    private CreateCoinService $createCoinService;
+    private UpdateCoinQuantityService $updateCoinQuantityService;
 
     public function __construct(
-        CoinRepositoryInterface $repository,
-        GetCoinByValueService $getCoinByValueService
+        GetCoinByValueService $getCoinByValueService,
+        CreateCoinService $createCoinService,
+        UpdateCoinQuantityService $updateCoinQuantityService
     ) {
-        $this->repository = $repository;
         $this->getCoinByValueService = $getCoinByValueService;
+        $this->createCoinService = $createCoinService;
+        $this->updateCoinQuantityService = $updateCoinQuantityService;
     }
 
     /**
      * Inserts a coin
      *
      * @param InsertCoinRequest $request
-     * @return boolean
+     * @throws CoinNotSavedException
+     * @return void
      */
     public function execute(
         InsertCoinRequest $request
-    ): bool {
+    ): void {
         $isOperationDone = false;
         $coinValue = $request->getCoin();
         $quantity = $request->getQuantity();
@@ -42,51 +44,13 @@ class InsertCoinService
         $coin = $this->getCoinByValueService->execute($coinValue);
 
         if (empty($coin)) {
-            $isOperationDone = $this->createNewCoin($coinValue, $quantity);
+            $isOperationDone = $this->createCoinService->execute($coinValue, $quantity);
         } else {
-            $coin = \reset($coin);
-            $isOperationDone = $this->updateCoinQuantityt(
-                $coin->getCoinId()->getValue(),
-                $coin->getCoinQuantity()->getValue(),
-                $quantity
-            );
+            $isOperationDone = $this->updateCoinQuantityService->execute($coinValue, $quantity);
         }
 
-        return $isOperationDone;
-    }
-
-    /**
-     * Creates a new coin
-     *
-     * @param float $coinValue
-     * @param integer $coinQuantity
-     * @return boolean
-     */
-    private function createNewCoin(float $coinValue, int $coinQuantity): bool
-    {
-        return $this->repository->saveCoin(
-            new Coin(
-                new CoinValue($coinValue),
-                new CoinQuantity($coinQuantity)
-            )
-        );
-    }
-
-    /**
-     * Updates the coin quantity
-     *
-     * @param float $coinId
-     * @param integer $coinQuantity
-     * @param integer $newCoinQuantity
-     * @return boolean
-     */
-    private function updateCoinQuantityt(
-        float $coinId,
-        int $coinQuantity,
-        int $newCoinQuantity
-    ): bool {
-        $coinId = new CoinId($coinId);
-        $coinQuantity = new CoinQuantity($coinQuantity + $newCoinQuantity);
-        return $this->repository->updateCoinQuantity($coinId, $coinQuantity);
+        if (!$isOperationDone) {
+            throw new CoinNotSavedException('The inserted coin has not been saved');
+        }
     }
 }
