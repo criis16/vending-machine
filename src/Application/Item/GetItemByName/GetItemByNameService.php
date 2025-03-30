5d13678e5e80a39116ccbc7c1d3a27dc2c1aa2f6
criis16 <cristian.ayala96@gmail.com>
2025-03-30 17:29:41 +0200

    Eliminar validación de nombre vacío en GetItemByNameService y ajustar pruebas unitarias

--- src/Application/Item/GetItemByName/GetItemByNameService.php
+++ src/Application/Item/GetItemByName/GetItemByNameService.php
@@ -4,7 +4,6 @@ namespace App\Application\Item\GetItemByName;
 
 use App\Domain\Item\Item;
 use App\Domain\Item\ItemName;
-use InvalidArgumentException;
 use App\Application\Item\Adapters\ItemAdapter;
 use App\Domain\Item\Repositories\ItemRepositoryInterface;
 use App\Infrastructure\Item\Repositories\InsertItemRequest;
@@ -30,13 +29,7 @@ class GetItemByNameService
      */
     public function execute(InsertItemRequest $request): array
     {
-        $name = $request->getName();
-
-        if (empty($name)) {
-            throw new InvalidArgumentException('The item name must be a valid value');
-        }
-
-        $itemName = new ItemName($name);
+        $itemName = new ItemName($request->getName());
 
         return \array_map(
             function (Item $item) {
